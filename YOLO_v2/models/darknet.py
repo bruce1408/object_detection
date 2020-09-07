9e36aeeaaca5c599e290031a3272eeead857431d
bruce1408 <summer56567@163.com>
2020-09-07 18:15:04 +0800

    yolo v2

--- YOLO_v2/models/darknet.py
+++ YOLO_v2/models/darknet.py
@@ -1,205 +1,114 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from util.network import WeightLoader
 import numpy as np
-import cv2
-import os
-import sys
+
 
 __all__ = ['darknet19', 'darknet53']
 
 
-class Conv_BN_LeakyReLU(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize, padding=0, stride=1, dilation=1):
-        super(Conv_BN_LeakyReLU, self).__init__()
-        self.convs = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, ksize, padding=padding, stride=stride, dilation=dilation),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.1, inplace=True)
-        )
+def conv_bn_leaky(in_channels, out_channels, kernel_size, return_module=False):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, kernel_size, )
+    ]
+
+
+
+def conv_bn_leaky(in_channels, out_channels, kernel_size, return_module=False):
+    padding = int((kernel_size - 1) / 2)
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding, bias=False),
+              nn.BatchNorm2d(out_channels),
+              nn.LeakyReLU(0.1, inplace=True)]
+
+    if return_module:
+        return nn.Sequential(*layers)
+    else:
+        return layers
+
+
+class GlobalAvgPool2d(nn.Module):
+    def __init__(self):
+        super(GlobalAvgPool2d, self).__init__()
 
     def forward(self, x):
-        return self.convs(x)
-
-
-class resblock(nn.Module):
-    def __init__(self, ch, nblocks=1):
-        super().__init__()
-        self.module_list = nn.ModuleList()
-        for _ in range(nblocks):
-            resblock_one = nn.Sequential(
-                Conv_BN_LeakyReLU(ch, ch // 2, 1),
-                Conv_BN_LeakyReLU(ch // 2, ch, 3, padding=1)
-            )
-            self.module_list.append(resblock_one)
-
-    def forward(self, x):
-        for module in self.module_list:
-            x = module(x) + x
+        N = x.data.size(0)
+        C = x.data.size(1)
+        H = x.data.size(2)
+        W = x.data.size(3)
+        x = F.avg_pool2d(x, (H, W))
+        x = x.view(N, C)
         return x
 
 
-class DarkNet_19(nn.Module):
+class Darknet19(nn.Module):
+    """
+    按照官网的darknet19来实现的网络结构,没有任何问题
+    """
+
+    cfg = {
+        'layer0': [32],
+        'layer1': ['M', 64],
+        'layer2': ['M', 128, 64, 128],
+        'layer3': ['M', 256, 128, 256],
+        'layer4': ['M', 512, 256, 512, 256, 512],
+        'layer5': ['M', 1024, 512, 1024, 512, 1024]
+    }
+
     def __init__(self, num_classes=1000):
-        print("Initializing the darknet19 network ......")
+        super(Darknet19, self).__init__()
+        self.in_channels = 3
 
-        super(DarkNet_19, self).__init__()
-        # backbone network : DarkNet-19
-        # output : stride = 2, c = 32
-        self.conv_1 = nn.Sequential(
-            Conv_BN_LeakyReLU(3, 32, 3, 1),
-            nn.MaxPool2d((2, 2), 2),
-        )
+        self.layer0 = self._make_layers(self.cfg['layer0'])
+        self.layer1 = self._make_layers(self.cfg['layer1'])
+        self.layer2 = self._make_layers(self.cfg['layer2'])
+        self.layer3 = self._make_layers(self.cfg['layer3'])
+        self.layer4 = self._make_layers(self.cfg['layer4'])
+        self.layer5 = self._make_layers(self.cfg['layer5'])
 
-        # output : stride = 4, c = 64
-        self.conv_2 = nn.Sequential(
-            Conv_BN_LeakyReLU(32, 64, 3, 1),
-            nn.MaxPool2d((2, 2), 2)
-        )
-
-        # output : stride = 8, c = 128
-        self.conv_3 = nn.Sequential(
-            Conv_BN_LeakyReLU(64, 128, 3, 1),
-            Conv_BN_LeakyReLU(128, 64, 1),
-            Conv_BN_LeakyReLU(64, 128, 3, 1),
-            nn.MaxPool2d((2, 2), 2)
-        )
-
-        # output : stride = 8, c = 256
-        self.conv_4 = nn.Sequential(
-            Conv_BN_LeakyReLU(128, 256, 3, 1),
-            Conv_BN_LeakyReLU(256, 128, 1),
-            Conv_BN_LeakyReLU(128, 256, 3, 1),
-        )
-
-        # output : stride = 16, c = 512
-        self.maxpool_4 = nn.MaxPool2d((2, 2), 2)
-        self.conv_5 = nn.Sequential(
-            Conv_BN_LeakyReLU(256, 512, 3, 1),
-            Conv_BN_LeakyReLU(512, 256, 1),
-            Conv_BN_LeakyReLU(256, 512, 3, 1),
-            Conv_BN_LeakyReLU(512, 256, 1),
-            Conv_BN_LeakyReLU(256, 512, 3, 1),
-        )
-
-        # output : stride = 32, c = 1024
-        self.maxpool_5 = nn.MaxPool2d((2, 2), 2)
-        self.conv_6 = nn.Sequential(
-            Conv_BN_LeakyReLU(512, 1024, 3, 1),
-            Conv_BN_LeakyReLU(1024, 512, 1),
-            Conv_BN_LeakyReLU(512, 1024, 3, 1),
-            Conv_BN_LeakyReLU(1024, 512, 1),
-            Conv_BN_LeakyReLU(512, 1024, 3, 1)
-        )
-
-        # self.conv_7 = nn.Conv2d(1024, 1000, 1)
-        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv = nn.Conv2d(self.in_channels, num_classes, kernel_size=1, stride=1)  # 最后一个卷积层
+        self.avgpool = GlobalAvgPool2d()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = self.conv_1(x)
-        x = self.conv_2(x)
-        x = self.conv_3(x)
-        C_4 = self.conv_4(x)
-        C_5 = self.conv_5(self.maxpool_4(C_4))
-        C_6 = self.conv_6(self.maxpool_5(C_5))
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        print(x.shape)
+        x = self.layer5(x)
 
-        # x = self.conv_7(C_6)
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # return x
-        return C_4, C_5, C_6
+        x = self.conv(x)  # [1, 1000, 7, 7]
+        x = self.avgpool(x)  # [1, 1000]
+        x = self.softmax(x)
 
+        return x
 
-class DarkNet_53(nn.Module):
-    """
-    YOLOv3 model module. The module list is defined by create_yolov3_modules function. \
-    The network returns loss values from three YOLO layers during training \
-    and detection results during test.
-    """
+    def _make_layers(self, layer_cfg):
+        layers = []
 
-    def __init__(self, num_classes=1000):
-        super(DarkNet_53, self).__init__()
-        # stride = 2
-        self.layer_1 = nn.Sequential(
-            Conv_BN_LeakyReLU(3, 32, 3, padding=1),
-            Conv_BN_LeakyReLU(32, 64, 3, padding=1, stride=2),
-            resblock(64, nblocks=1)
-        )
-        # stride = 4
-        self.layer_2 = nn.Sequential(
-            Conv_BN_LeakyReLU(64, 128, 3, padding=1, stride=2),
-            resblock(128, nblocks=2)
-        )
-        # stride = 8
-        self.layer_3 = nn.Sequential(
-            Conv_BN_LeakyReLU(128, 256, 3, padding=1, stride=2),
-            resblock(256, nblocks=8)
-        )
-        # stride = 16
-        self.layer_4 = nn.Sequential(
-            Conv_BN_LeakyReLU(256, 512, 3, padding=1, stride=2),
-            resblock(512, nblocks=8)
-        )
-        # stride = 32
-        self.layer_5 = nn.Sequential(
-            Conv_BN_LeakyReLU(512, 1024, 3, padding=1, stride=2),
-            resblock(1024, nblocks=4)
-        )
+        # set the kernel size of the first conv block = 3
+        kernel_size = 3
+        for v in layer_cfg:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += conv_bn_leaky(self.in_channels, v, kernel_size)
+                kernel_size = 1 if kernel_size == 3 else 3
+                self.in_channels = v
 
-        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.fc = nn.Linear(1024, num_classes)
+        return nn.Sequential(*layers)
 
-    def forward(self, x, targets=None):
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        C_3 = self.layer_3(x)
-        C_4 = self.layer_4(C_3)
-        C_5 = self.layer_5(C_4)
-
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # x = self.fc(x)
-
-        return C_3, C_4, C_5
+    # very ugly code !! need to reconstruct
+    def load_weights(self, weights_file):
+        weights_loader = WeightLoader()
+        weights_loader.load(self, weights_file)
 
 
-def darknet19(pretrained=False, hr=False, **kwargs):
-    """Constructs a darknet-19 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = DarkNet_19()
-    if pretrained:
-        print('Loading the pretrained model ...')
-        path_to_dir = os.path.dirname(os.path.abspath(__file__))
-        if hr:
-            print('Loading the hi-res darknet19-448 ...')
-            model.load_state_dict(
-                torch.load(path_to_dir + '/weights/darknet19_hr_75.52_92.73.pth', map_location='cuda'), strict=False)
-        else:
-            print('Loading the darknet19 ...')
-            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet19_72.96.pth', map_location='cuda'),
-                                  strict=False)
-    return model
-
-
-def darknet53(pretrained=False, hr=False, **kwargs):
-    """Constructs a darknet-53 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = DarkNet_53()
-    if pretrained:
-        print('Loading the pretrained model ...')
-        path_to_dir = os.path.dirname(os.path.abspath(__file__))
-        if hr:
-            print('Loading the hi-res darknet53-448 ...')
-            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet53_hr_77.76.pth', map_location='cuda'),
-                                  strict=False)
-        else:
-            print('Loading the darknet53 ...')
-            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet53_75.42.pth', map_location='cuda'),
-                                  strict=False)
-    return model
+if __name__ == '__main__':
+    x = torch.rand((1, 3, 416, 416))
+    model = Darknet19()
+    out = model(x)
+    # print(model)
+    print(out.size())
--- YOLO_v2/models/yolo_v2_net.py
+++ YOLO_v2/models/yolo_v2_net.py
@@ -4,8 +4,17 @@ import torch.nn as nn
 from layers import ReorgLayer
 from darknet import darknet19
 from darknet import Conv_BN_LeakyReLU
-from torchsummary import summary
 
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from torchsummary import summary
+from config import config as cfg
+from darknet import Darknet19
+from darknet import conv_bn_leaky
+# from loss import build_target, yolo_loss
+from util.network import ReorgLayer
+from tensorboardX import SummaryWriter
 
 class ReorgLayer(nn.Module):
     def __init__(self, stride=2):
@@ -36,42 +45,103 @@ class ReorgLayer(nn.Module):
         return x
 
 
+
+
 class Yolov2(nn.Module):
-    num_classses = 20
+
+    num_classes = 20
     num_anchors = 5
 
-    def __init__(self, device, input_size=None, num_classes=20, trainable=False, conf_thresh=0.001, nms_thresh=0.5,
-                 anchor_size=None, hr=False):
+    def __init__(self, classes=None, weights_file=False):
         super(Yolov2, self).__init__()
-        self.device = device
-        self.num_classses = num_classes
-        self.trainable = trainable
-        self.input_size = input_size
-        self.nms_thresh = nms_thresh
-        self.conf_thresh = conf_thresh
-        self.anchor_size = torch.tensor(anchor_size)
-        self.anchor_number = len(anchor_size)
-        self.stride = 32
-        self.hr = hr
+        if classes:
+            self.num_classes = len(classes)
 
-        if not trainable:
-            self.grid_cell, self.all_anchor_wh = self.set_init(input_size)
+        darknet19 = Darknet19()
 
-        # backbone darknet-19
-        self.backbone = darknet19(pretrained=trainable, hr=hr)
+        # 模型加载部分
+        if weights_file:
+            print('darknet-19 load pretrained weight from {}'.format(weights_file))
+            darknet19.load_weights(weights_file)
+            print('pretrained weight loaded!')
 
-        # detection
-        self.conv1 = nn.Sequential(
-            Conv_BN_LeakyReLU(1024, 1024, 3, 1),
-            Conv_BN_LeakyReLU(1024, 1024, 3, 1)
-        )
+        # darknet backbone, output = [b, 512, 26, 26]
+        self.conv1 = nn.Sequential(darknet19.layer0,
+                                   darknet19.layer1,
+                                   darknet19.layer2,
+                                   darknet19.layer3,
+                                   darknet19.layer4)
 
-        self.route_layer = Conv_BN_LeakyReLU(512, 64, 1)
-        self.reorg = ReorgLayer(stride=2)
-        self.conv2 = Conv_BN_LeakyReLU(1280, 1024, 3, 1)
+        # output = [b, 1024, 13, 13]
+        self.conv2 = darknet19.layer5
 
-        # prediction layer
-        self.pred = nn.Conv2d(1280, 1024, 3, 1)
+        # detection layers
+        self.conv3 = nn.Sequential(conv_bn_leaky(1024, 1024, kernel_size=3, return_module=True),
+                                   conv_bn_leaky(1024, 1024, kernel_size=3, return_module=True))
+
+        self.downsampler = conv_bn_leaky(512, 64, kernel_size=1, return_module=True)
+
+        self.conv4 = nn.Sequential(conv_bn_leaky(1280, 1024, kernel_size=3, return_module=True),
+                                   nn.Conv2d(1024, (5 + self.num_classes) * self.num_anchors, kernel_size=1))
+
+        self.reorg = ReorgLayer()
+
+    def forward(self, x, training=False):
+        """
+        x: Variable
+        gt_boxes, gt_classes, num_boxes: Tensor
+        """
+        x = self.conv1(x)
+        shortcut = self.reorg(self.downsampler(x))  # [1, 256, 13, 13]
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = torch.cat([shortcut, x], dim=1)
+        out = self.conv4(x)
+
+        if cfg.debug:
+            print('check output', out.view(-1)[0:10])
+
+        # out -- tensor of shape (B, num_anchors * (5 + num_classes), H, W)
+        bsize, _, h, w = out.size()
+
+        # 5 + num_class tensor represents (t_x, t_y, t_h, t_w, t_c) and (class1_score, class2_score, ...)
+        # reorganize the output tensor to shape (B, H * W * num_anchors, 5 + num_classes)
+        # [batch, 13*13*5, 25]
+        out = out.permute(0, 2, 3, 1).contiguous().view(bsize, h * w * self.num_anchors, 5 + self.num_classes)
+
+        # activate the output tensor
+        # `sigmoid` for t_x, t_y, t_c; `exp` for t_h, t_w;
+        # `softmax` for (class1_score, class2_score, ...)
+
+        xy_pred = torch.sigmoid(out[:, :, 0:2])
+        hw_pred = torch.exp(out[:, :, 2:4])
+        conf_pred = torch.sigmoid(out[:, :, 4:5])
+
+        class_pred = out[:, :, 5:]
+        delta_pred = torch.cat([xy_pred, hw_pred], dim=-1)  # [batch_size, h*w*5, 4]
+        class_score = F.softmax(class_pred, dim=-1)
+
+        if training:
+            return delta_pred, conf_pred, class_pred, h, w
+        else:
+            return delta_pred, conf_pred, class_score
+
+
+if __name__ == '__main__':
+    model = Yolov2()
+    if torch.cuda.is_available():
+        model.cuda()
+        x = torch.rand((1, 3, 416, 416)).to('cuda')
+        out = model(x, False)
+        delta_pred, conf_pred, class_pred = out
+        print('delta_pred size:', delta_pred.size())
+        print('conf_pred size:', conf_pred.size())
+        print('class_pred size:', class_pred.size())
+        summary(model, (3, 416, 416))
+        with SummaryWriter(comment="yolov2") as w:
+            w.add_graph(model, x)
+    else:
+        summary(model, (3, 224, 224))
 
 
 
