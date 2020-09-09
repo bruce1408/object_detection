2297afee65ae5e4971ffb496ee0cf8c523bdd8cb
bruce1408 <summer56567@163.com>
2020-09-09 19:51:04 +0800

    yolo v2 datasets

--- CustomData/pascal_voc.py
+++ CustomData/pascal_voc.py
@@ -1,10 +1,3 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
 from __future__ import print_function
 from __future__ import absolute_import
 
