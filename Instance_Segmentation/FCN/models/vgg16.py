4917ada754d5683ed435a724a486672880a29df7
bruce1408 <summer56567@163.com>
2020-09-25 01:17:30 +0800

    datasets

---
+++ Instance_Segmentation/FCN/models/vgg16.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from torchvision.models import vgg16
+
+
+class VGG(nn.Module):
+    def __init__(self, pretrained=True):
+        super(VGG, self).__init__()
+
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.relu1_1 = nn.ReLU(inplace=True)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.relu2_1 = nn.ReLU(inplace=True)
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.relu2_2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # conv3 1/8
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.relu3_1 = nn.ReLU(inplace=True)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.relu3_2 = nn.ReLU(inplace=True)
+        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.relu3_3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # conv4 1/16
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.relu4_1 = nn.ReLU(inplace=True)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu4_2 = nn.ReLU(inplace=True)
+        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu4_3 = nn.ReLU(inplace=True)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # conv5 1/32
+        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu5_1 = nn.ReLU(inplace=True)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu5_2 = nn.ReLU(inplace=True)
+        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.relu5_3 = nn.ReLU(inplace=True)
+        self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        if pretrained:
+            pretrained_model = vgg16(pretrained=True)
+            pretrained_state_dict = pretrained_model.state_dict()
+            net = self.state_dict()
+            for k in pretrained_state_dict.keys():
+                if k in net.keys():
+                    net[k] = pretrained_state_dict[k]
+            self.load_state_dict(net)
+
+    def forward(self, x):
+        x = self.relu1_1(self.conv1_1(x))
+        x = self.relu1_2(self.conv1_2(x))
+        x = self.pool1(x)
+        pool1 = x
+
+        x = self.relu2_1(self.conv2_1(x))
+        x = self.relu2_2(self.conv2_2(x))
+        x = self.pool2(x)
+        pool2 = x
+
+        x = self.relu3_1(self.conv3_1(x))
+        x = self.relu3_2(self.conv3_2(x))
+        x = self.relu3_3(self.conv3_3(x))
+        x = self.pool3(x)
+        pool3 = x
+
+        x = self.relu4_1(self.conv4_1(x))
+        x = self.relu4_2(self.conv4_2(x))
+        x = self.relu4_3(self.conv4_3(x))
+        x = self.pool4(x)
+        pool4 = x
+
+        x = self.relu5_1(self.conv5_1(x))
+        x = self.relu5_2(self.conv5_2(x))
+        x = self.relu5_3(self.conv5_3(x))
+        x = self.pool5(x)
+        pool5 = x
+
+        return pool1, pool2, pool3, pool4, pool5
+
+
+if __name__ == "__main__":
+    net = VGG()
+    if torch.cuda.is_available():
+        net.to('cuda')
+        x = torch.rand((2, 3, 224, 224)).to('cuda')
+        print(net(x)[-1].shape)
+    else:
+        x = torch.rand((2, 3, 224, 224))
+        print(net(x)[-1])
+
+
+
+
+
+
+
+
+
+
+
+
